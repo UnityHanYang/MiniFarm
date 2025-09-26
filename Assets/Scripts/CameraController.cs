5a2bf8f1785dd00a3bd1f6aeb129cf931481d15e
UnityHanYang <UnityHanYang@gmail.com>
2025-09-26 12:52:37 +0900

    edit

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,6 +7,10 @@ public class CameraController : MonoBehaviour
 
     private float speed = 3f;
     private Vector2 lastTouchPosition;
+    private float minX = 3f;
+    private float maxX = 5.1f;
+    private float minY = 3f;
+    private float maxY = 5.4f;
 
     private void Update()
     {
@@ -25,10 +29,27 @@ public class CameraController : MonoBehaviour
         else if (Input.GetTouch(0).phase == TouchPhase.Moved)
         {
             isMove = true;
+
             Vector3 delta = Input.GetTouch(0).position - lastTouchPosition;
             delta = delta.normalized;
-            transform.Translate(-delta.x * speed * Time.deltaTime, -delta.y * speed * Time.deltaTime, 0);
-            lastTouchPosition = Input.GetTouch(0).position;
+            if(transform.position.x >= 3 && transform.position.x <= 5.1 && transform.position.y <= 5.4 && transform.position.y >= 3)
+            {
+                transform.Translate(-delta.x * speed * Time.deltaTime, -delta.y * speed * Time.deltaTime, 0);
+                lastTouchPosition = Input.GetTouch(0).position;
+            }
+            else
+            {
+                transform.position = StandardPos();
+            }
         }
+    }
+
+    private Vector3 StandardPos()
+    {
+        Vector3 standardVec = transform.position;
+        standardVec.x = Mathf.Clamp(standardVec.x, 3.1f, 6f);
+        standardVec.y = Mathf.Clamp(standardVec.y, 3.1f, 6.3f);
+
+        return standardVec;
     }
 }
